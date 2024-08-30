a35152ba81c9ebdf327b73e9afdc3bfe09608e85
AarFeE <andres.jimenezv@hotmail.com>
2024-08-30 15:17:33 -0500

    First Commit

---
+++ src/main/java/riwi/workshop/number_three/NumberThreeApplication.java
@@ -0,0 +1,13 @@
+package riwi.workshop.number_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/riwi/workshop/number_three/NumberThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package riwi.workshop.number_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
