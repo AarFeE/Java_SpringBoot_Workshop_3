1ae8060ac60a3c67454207b93ad7da30412febb3
JEscobar07 <EscobarjonathanM@outlook.es>
2024-08-30 21:28:29 -0500

    delete cretate interface

--- src/main/java/riwi/workshop/number_three/controllers/generic/Create.java
+++
@@ -1,7 +0,0 @@
-package riwi.workshop.number_three.controllers.generic;
-
-import org.springframework.http.ResponseEntity;
-
-public interface Create<EntityRequest, Entity>{
-    public ResponseEntity<Entity> create(EntityRequest entity);
-}
